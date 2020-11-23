0bdd7d997c380dbe49b01980b521a8d4b6809705
Damir Petrov <petrovdamir2235@gmail.com>
2020-11-23 18:34:28 +0300

    Parse java files from args to .dot files

--- source/java-parser/app/src/main/java/parser/App.java
+++ source/java-parser/app/src/main/java/parser/App.java
@@ -1,14 +1,35 @@
 package parser;
 
-import parser.SomeClass;
-import parser.Analyzer.*;
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.printer.DotPrinter;
 
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.*;
 
 public class App {
-  private static final String FILE_PATH = "app/src/main/java/parser/SomeClass.java";
-  public static void main(String[] args) throws FileNotFoundException {
-    new Analyzer().analyze(FILE_PATH);
+  public static void main(String[] args) throws Exception {
+    for (int i = 0; i < args.length; ++i) {
+      FileWriter writer;
+      try {
+        writer = new FileWriter("../uast" + i + ".dot");
+      } catch (IOException e) {
+          System.err.println("Can't write in file");
+          e.printStackTrace();
+          throw e;
+      }
+
+      try {
+        CompilationUnit cu = StaticJavaParser.parse(new FileInputStream(args[i]));
+        writer.write(new DotPrinter(true).output(cu) + "\n");
+      } catch (Exception e) {
+        System.err.println("Problems with parsing " + i + "th file: " + args[i]);
+        e.printStackTrace();
+        throw e;
+      }
+
+      writer.flush();
+      writer.close();
+      System.out.println("File " + args[i] + " is presented in uast" + i + ".dot");
+    }
   }
 }
