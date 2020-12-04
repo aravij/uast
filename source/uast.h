5676107d2b1c9a91724113ad6d2410ccbf72cc77
elevankoff <yegor.levankov@yandex.com>
2020-12-04 17:56:04 +0300

    [8] Add uast-build and related functions

--- source/uast.h
+++ source/uast.h
@@ -1,13 +1,44 @@
 #pragma once
 
 #include <string>
-#include "node.h"
-
+#include <unordered_set>
+#include <unordered_map>
 #include "cpp_parser.h"
+#include "node.h"
 
 // #include "java_parser.h"
 // #include "python_parser.h"
 
 namespace uast {
-std::string Stub();
+    namespace {
+        enum PLanguage { Cpp, Java, Python, None };
+
+        std::unordered_map<PLanguage, std::unordered_set<std::string>> extensions = {
+            {Cpp, {".CPP", ".cpp", ".cxx", ".CXX", ".hpp", ".hxx", ".Hxx", ".HXX"}},
+            {Java, {".java"}},
+            {Python, {".py", ".py3"}}
+        };
+
+        PLanguage detectLang(const std::string& filePath);
+
+        std::string getRunner(const PLanguage& pLang,
+                              const std::string& javaVmPath,
+                              const std::string& pythonVmPath);
+
+        struct DotVertex {
+            std::string type;
+            std::string value;
+        };
+
+        struct DotEdge {
+            std::string name;
+        };
+    }
+
+
+    Node build(const std::string& filePath,
+               std::string& javaVmPath   = (std::string&)"",
+               std::string& pythonVmPath = (std::string&)"");
+
+    std::string Stub();
 }
